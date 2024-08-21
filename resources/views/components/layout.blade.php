34a190b1bd7445b5abc468a460f9f3a7b5db43e4
shakhawathh <shakhawathh55@gmail.com>
2024-08-21 14:39:12 +0600

    tailwind

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,29 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job</title>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     
 </head>
-<body>
-    <div>
+<body class="bg-red-500 text-white">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
 
-    <nav>
-        <div>
-            <a href="">
-                <img src="{{ Vite::asset('resources/images/logo.svg') }}">
-            </a>
-            
-        </div>
-        <div>
-            links
-        </div>
-        <div>
-            post a job
-        </div>
-    </nav>
-    <main>
-        {{ $slot }}
-    </main>
+            <div class="space-x-6 font-bold">
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+
+            <div>
+                <a href="">Post a Job</a>
+            </div>
+        </nav>
+
+        <main class="mt-10 max-w-[986px] mx-auto">
+            {{ $slot }}
+        </main>
     </div>
+
 </body>
 </html>
