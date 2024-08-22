0c15a196cf5fc06c0fdb990e7a486768755b10ec
shakhawathh <shakhawathh55@gmail.com>
2024-08-22 19:31:11 +0600

    database fetching

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
-<?php
+ <?php
 
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\JobController;
+    use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', [JobController::class, 'index']);
--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,5 +1,23 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
+use App\Models\Employer;
+use App\Models\Job;
+
+it('belongs to an employer', function () {
+    //Arrange
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    //Act adn Assert
+    expect($job->employer->is($employer))->toBeTrue();
+});
+
+
+it('can have tags', function () {
+
+    $job = Job::factory()->create();
+    $job->tag('Frontend');
+    expect($job->tags)->toHaveCount(1);
 });
